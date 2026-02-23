9060e6c56dccd43dde25d267982c9438d2f5382b
rezafauzan <rezafauzan593@gmail.com>
2026-02-23 10:54:59 +0700

    Feat: add query to show movies with rankscore between 5 and 7 with publish year between 2004 to 2006

--- query1.sql
+++ query1.sql
@@ -1,3 +1,5 @@
 SELECT name,year FROM movies WHERE year > 2000;
 
 SELECT first_name FROM actors WHERE first_name ILIKE '%s'
+
+SELECT name, year, rankscore FROM movies WHERE rankscore > 5 AND rankscore < 7 AND year > 2003 AND year < 2006;
