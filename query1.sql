0294695d20b2fd9ccc6b330dc57da9602824589b
rezafauzan <rezafauzan593@gmail.com>
2026-02-23 10:55:25 +0700

    Feat: add query to count movies with rankscore = 6

--- query1.sql
+++ query1.sql
@@ -3,3 +3,5 @@ SELECT name,year FROM movies WHERE year > 2000;
 SELECT first_name FROM actors WHERE first_name ILIKE '%s'
 
 SELECT name, year, rankscore FROM movies WHERE rankscore > 5 AND rankscore < 7 AND year > 2003 AND year < 2006;
+
+SELECT COUNT(id) FROM movies where rankscore = 6
